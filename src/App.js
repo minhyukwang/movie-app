7e5806886c1aded09059b7ef6fcd1bfa5f3706d4
Minhyuk <minhyukit@gmail.com>
2021-10-24 12:01:05 +0900

    211024 Router and Github Deploy

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-@@ -23,10 +24,14 @@ class App extends React.Component {
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
-      <section class="container">
-        {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div class="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-@@ -36,7 +41,9 @@ class App extends React.Component {
-                poster={movie.medium_cover_image}
-              />
-            ))}
-      </div>
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </BrowserRouter>
+  );
 }
+
+export default App;
